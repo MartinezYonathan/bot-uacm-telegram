e200e3dcc1712282b88deca4a17a18275ba67724
yonathan martinez <j-ma@hotmail.es>
2021-01-24 00:43:59 -0600

    se cambio forma de llamar a api de telegram

--- bot.py
+++ bot.py
@@ -1,92 +1,59 @@
-import datetime
-import telepot
-import os
-import time
+# !/usr/bin/env python
+# pylint: disable=W0603
+"""Simple Bot to reply to Telegram messages.
+This is built on the API wrapper, see rawapibot.py to see the same example built
+on the telegram.ext bot framework.
+This program is dedicated to the public domain under the CC0 license.
+"""
+import logging
+from typing import NoReturn
+from time import sleep
 
-global reg
-reg=[]
+import telegram
+from telegram.error import NetworkError, Unauthorized
 
-def handle(msg):
-    #registros=[]
-    chat_id = msg['chat']['id']
-    command = msg['text']
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-    def start():
-        try:
-            bot.sendMessage(chat_id,("Bot de Parqueo"))
-            bot.sendMessage(chat_id,("Puede realizar lo siguiente: \n"))
-            bot.sendMessage(chat_id,("1) /ingresar (placa carro)\nIngresa carro al parqueo"))
-            bot.sendMessage(chat_id,("2) /consultar (placa carrp)\nConsultar si el carro ha entrado al parqueo"))
-            bot.sendMessage(chat_id,("3) /borrar (placa)\nCarro sale del parqueo"))
-        except(TypeError, NameError, ValueError):
-            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-    def Ingresar_Articulo(command):
-        try:
-            datos=command.split()
-            placa=int(datos[1])
-            if len(reg)<10:
-                reg.append(placa)
-                bot.sendMessage(chat_id, "Aun hay parqueo disponible")
-                bot.sendMessage(chat_id, ("Entro el Carro"))
-                bot.sendMessage(chat_id, reg)
-            else:
-                bot.sendMessage(chat_id ,"ya no hay parqueo")
-        except(TypeError, NameError, ValueError):
-            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
 
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-    def Consultar_Articulo(command):
-        try:
-            comp=command.split()
-            c=int(comp[1])
-            if c in reg:
-                bot.sendMessage(chat_id,"El carro si esta en el parqueo")
-            else:
-                bot.sendMessage(chat_id, "El carro no esta en el parqueo\nLe puso llave :D")
-        except(TypeError, NameError, ValueError):
-            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
-            
-    #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-    def Borrar(command):#funcion para borrar un articulo
-        try:
-            comp=command.split()
-            c=int(comp[1])
-            if c in reg:
-                reg.remove(c)
-                bot.sendMessage(chat_id,"El carro ha salido del parqueo")
-            else:
-                bot.sendMessage(chat_id, "El carro no esta en el parqueo\nLe puso llave :D")
-        except(TypeError, NameError, ValueError):
-            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-    lista1=['/ingresar','/consultar','/borrar']
-    div=command.split()
-    comparacion = []
-    for item in lista1:
-        if item in div:
-            comparacion.append(item)
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+UPDATE_ID = None
+
+
+def main() -> NoReturn:
+    """Run the bot."""
+    global UPDATE_ID
+    # Telegram Bot Authorization Token
+    TOKEN = os.getenv("TOKEN")
+    bot = telegram.Bot(TOKEN)
+
+    # get the first pending update_id, this is so we can skip over it in case
+    # we get an "Unauthorized" exception.
     try:
-        if command==("/start"):#comparo y decido que funcion se ejecutara, para el caso de /start, como no contiene valores de entrada de usuario va directo
-            start()#la funcion tiene los valores de entrada del id de la persona y el objeto bot, para no utilizaro 2 veces
-        elif comparacion[0]==("/ingresar"):#a esta funcion le envio command porque las cosas llegan por ejemplo
-            Ingresar_Articulo(command)#/Ingresar 123(codigo) 25(cantidad) 100(precio) zapatos(nombre)
-        elif comparacion[0]==("/consultar"):#para diferenciar eso hago lo del .split()
-            Consultar_Articulo(command)#haci con todas las funciones similares
-        elif comparacion[0]==("/borrar"):
-            Borrar(command)
-    except(IndexError):
-        bot.sendMessage(chat_id, ("Ingreso una funcion no valida"))
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+        UPDATE_ID = bot.get_updates()[0].update_id
+    except IndexError:
+        UPDATE_ID = None
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    while True:
+        try:
+            echo(bot)
+        except NetworkError:
+            sleep(1)
+        except Unauthorized:
+            # The user has removed or blocked the bot.
+            UPDATE_ID += 1  # type: ignore[operator]
 
 
-TOKEN = os.getenv("TOKEN")
-print (TOKEN)
-bot = telepot.Bot(TOKEN)
-bot.message_loop(handle)
-print ('Estoy escuchando...')
+def echo(bot: telegram.Bot) -> None:
+    """Echo the message the user sent."""
+    global UPDATE_ID
+    # Request updates after the last update_id
+    for update in bot.get_updates(offset=UPDATE_ID, timeout=10):
+        UPDATE_ID = update.update_id + 1
+
+        if update.message:  # your bot can receive updates without messages
+            if update.message.text:  # not all messages contain text
+                # Reply to the message
+                update.message.reply_text(update.message.text)
 
 
-while 1:
-     time.sleep(10)
+if __name__ == '__main__':
+    main()
