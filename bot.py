79c7d8eaa329c52dd6d1247f7c5a7c277e11733b
yonathan martinez <j-ma@hotmail.es>
2021-01-24 00:07:34 -0600

    se agrego telepot a requirements

--- bot.py
+++ bot.py
@@ -1,48 +1,91 @@
-import logging
+import datetime
+import telepot
 import os
-import random
-import sys
+import time
 
-from telegram.ext import Updater, CommandHandler
+global reg
+reg=[]
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
+def handle(msg):
+    #registros=[]
+    chat_id = msg['chat']['id']
+    command = msg['text']
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    def start():
+        try:
+            bot.sendMessage(chat_id,("Bot de Parqueo"))
+            bot.sendMessage(chat_id,("Puede realizar lo siguiente: \n"))
+            bot.sendMessage(chat_id,("1) /ingresar (placa carro)\nIngresa carro al parqueo"))
+            bot.sendMessage(chat_id,("2) /consultar (placa carrp)\nConsultar si el carro ha entrado al parqueo"))
+            bot.sendMessage(chat_id,("3) /borrar (placa)\nCarro sale del parqueo"))
+        except(TypeError, NameError, ValueError):
+            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    def Ingresar_Articulo(command):
+        try:
+            datos=command.split()
+            placa=int(datos[1])
+            if len(reg)<10:
+                reg.append(placa)
+                bot.sendMessage(chat_id, "Aun hay parqueo disponible")
+                bot.sendMessage(chat_id, ("Entro el Carro"))
+                bot.sendMessage(chat_id, reg)
+            else:
+                bot.sendMessage(chat_id ,"ya no hay parqueo")
+        except(TypeError, NameError, ValueError):
+            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
 
-mode = os.getenv("MODE")
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    def Consultar_Articulo(command):
+        try:
+            comp=command.split()
+            c=int(comp[1])
+            if c in reg:
+                bot.sendMessage(chat_id,"El carro si esta en el parqueo")
+            else:
+                bot.sendMessage(chat_id, "El carro no esta en el parqueo\nLe puso llave :D")
+        except(TypeError, NameError, ValueError):
+            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
+            
+    #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    def Borrar(command):#funcion para borrar un articulo
+        try:
+            comp=command.split()
+            c=int(comp[1])
+            if c in reg:
+                reg.remove(c)
+                bot.sendMessage(chat_id,"El carro ha salido del parqueo")
+            else:
+                bot.sendMessage(chat_id, "El carro no esta en el parqueo\nLe puso llave :D")
+        except(TypeError, NameError, ValueError):
+            bot.sendMessage(chat_id,"No envie una cadena donde vaa un entero")
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    lista1=['/ingresar','/consultar','/borrar']
+    div=command.split()
+    comparacion = []
+    for item in lista1:
+        if item in div:
+            comparacion.append(item)
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+    try:
+        if command==("/start"):#comparo y decido que funcion se ejecutara, para el caso de /start, como no contiene valores de entrada de usuario va directo
+            start()#la funcion tiene los valores de entrada del id de la persona y el objeto bot, para no utilizaro 2 veces
+        elif comparacion[0]==("/ingresar"):#a esta funcion le envio command porque las cosas llegan por ejemplo
+            Ingresar_Articulo(command)#/Ingresar 123(codigo) 25(cantidad) 100(precio) zapatos(nombre)
+        elif comparacion[0]==("/consultar"):#para diferenciar eso hago lo del .split()
+            Consultar_Articulo(command)#haci con todas las funciones similares
+        elif comparacion[0]==("/borrar"):
+            Borrar(command)
+    except(IndexError):
+        bot.sendMessage(chat_id, ("Ingreso una funcion no valida"))
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+
+
 TOKEN = os.getenv("TOKEN")
-if mode == "dev":
-    def run(updater):
-        updater.start_polling()
-elif mode == "prod":
-    def run(updater):
-        PORT = int(os.environ.get("PORT", "8443"))
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-        updater.start_webhook(listen="0.0.0.0",
-                              port=PORT,
-                              url_path=TOKEN)
-        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
-else:
-    logger.error("No MODE specified!")
-    sys.exit(1)
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle)
+print ('Estoy escuchando...')
 
 
-def start_handler(bot, update):
-    logger.info("User {} started bot".format(update.effective_user["id"]))
-    update.message.reply_text("Hello from Python!\nPress /random to get random number")
-
-
-def random_handler(bot, update):
-    number = random.randint(0, 10)
-    logger.info("User {} randomed number {}".format(update.effective_user["id"], number))
-    update.message.reply_text("Random number: {}".format(number))
-
-
-if __name__ == '__main__':
-    logger.info("Starting bot")
-    updater = Updater(TOKEN)
-
-    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
-    updater.dispatcher.add_handler(CommandHandler("random", random_handler))
-
-    run(updater)
+while 1:
+     time.sleep(10)
