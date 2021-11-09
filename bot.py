ad055c769b6998d7fa7ac6db1501d6a5fc03b8d1
Yonathan Martinez <33378318+MartinezYonathan@users.noreply.github.com>
2021-11-08 23:06:55 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,7 @@ import logging
 import os
 import random
 
-#TOKEN = os.getenv("TOKEN")
-TOKEN = "1596253229:AAGUFRYRC2uhgpbymVU3VXR_ckUw0sdgWBI"
+TOKEN = os.getenv("TOKEN")
 updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
